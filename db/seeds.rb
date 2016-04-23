101e6116f9d673940f56a510ebcc4fad24bc65a8
Stephen Doolan <doolan12@hotmail.com>
2016-04-23 23:59:38 +0100

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,15 +13,18 @@
 #Tour.create(:user_id => u.id , :name => "Gallery Tour" , :description => "2 hours trip to the central gallery" , :price => 100)
 
 
- #u = User.create(:first_name => "John" , :last_name => "Doolan" , :email => "john@gmail.com" , :password => "john" , :rate_per_hour => 20)
+ #u = User.create(:first_name => "david" , :last_name => "Dean" , :email => "dean@gmail.com" , :password => "dean" , :rate_per_hour => 20)
+ #u = User.create(:first_name => "Janice" , :last_name => "kelly" , :email => "jk@gmail.com" , :password => "jkely" , :rate_per_hour => 30)
+ #u = User.create(:first_name => "Mary" , :last_name => "Keating" , :email => "mkeating@gmail.com" , :password => "mkeating" , :rate_per_hour => 50)
+ #u = User.create(:first_name => "Paul" , :last_name => "Walsh" , :email => "walsh@gmail.com" , :password => "pwalsh" , :rate_per_hour => 60)
 
- #Tour.create(:user_id => u.id , :name => "Jurassic Park Tour" , :description => "2 hours trip to the Jurassic park" , :price => 500)
- #Tour.create(:user_id => u.id , :name => "Train Tour" , :description => "2 hours trip to the train station" , :price => 200)
-
-
-u = User.find_by_email("stephen@gmail.com")
-u.add_role :guide
-
-
-u = User.find_by_email("john@gmail.com")
-u.add_role :guide
+ Tour.create(:user_id => 9 , :name => "Dublin Mueseem" , :description => "view some of the oldest and most valuable artifacts in ireland" , :price => 100)
+ Tour.create(:user_id => 9 , :name => "Croke Park" , :description => "Complete tour of irelands biggest stadium" , :price => 150)
+ Tour.create(:user_id => 11 , :name => "Wax World" , :description => "celebrity wax works" , :price => 125)
+ Tour.create(:user_id => 11 , :name => "Viking Tour" , :description => "2 hours trip with viking " , :price => 200)
+ Tour.create(:user_id => 12 , :name => "Aviva Stadium" , :description => "complete tour of the Aviva " , :price => 60)
+ Tour.create(:user_id => 12 , :name => "Sea World Tour" , :description => "see some of the greatest animals who live under the sea" , :price => 95)
+ Tour.create(:user_id => 13 , :name => "Train Tour" , :description => "2 hours trip to the train station" , :price => 50)
+ Tour.create(:user_id => 13 , :name => "1916 events " , :description => "covers all major events of the historical year" , :price => 200)
+ Tour.create(:user_id => 6 , :name => "bus Tour" , :description => "Open top bus tour through dublin" , :price => 100)
+  Tour.create(:user_id => 6 , :name => "Car Mueseem" , :description => "view the most valuable car ever created" , :price => 140)
